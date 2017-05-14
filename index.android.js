a5080054a0202067a05459adc9f4bf34df3b2c44
jonyroy <jonyroy16@gmail.com>
2017-05-14 11:11:40 +0600

    updated

--- index.android.js
+++ index.android.js
@@ -9,10 +9,7 @@ import { AppRegistry,
          StyleSheet,
          Text,
          View,
-         Image,
-         TextInput,
          TouchableHighlight,
-         ListView,
         } from 'react-native';
 import FormatTime from 'minutes-seconds-milliseconds';
 
@@ -22,25 +19,21 @@ export class stopwatch extends Component {
     this.state={
         timeElapsed: null,
         startTime: null,
-        running: false,
-        flag: false,
+        isRunning: false,
         timer: null,
-        timer2: null,
         totalElapsedTime:null,
       };
     this.startPress = this.handleStartPress.bind(this);
     this.resetPress=this.resetPress.bind(this);
   }
 
-
-
 startStopButton(){
   return(
     <View style={{flexDirection: 'row',justifyContent: 'space-around',paddingTop: 0,paddingBottom: 0,}}>
       <TouchableHighlight underlayColor='red' onPress={this.startPress} style={styles.button}>
         <View>
-          <Text style={{fontSize:20,textAlign:'center'}}>
-            {this.state.running? 'STOP': 'START'}
+          <Text style={{fontSize:20}}>
+            {this.state.isRunning? 'STOP': 'START'}
           </Text>
         </View>
       </TouchableHighlight>
@@ -55,54 +48,53 @@ startStopButton(){
 
 resetPress() {
   clearInterval(this.interval);
-  this.setState({timeElapsed:null,running:false,flag:false,timer:null,timer2:null});
+  this.setState({
+    timeElapsed:null,
+    isRunning:false,
+    timer:null,
+    totalElapsedTime:null
+  });
 }
-
 
 handleStartPress() {
-  if(this.state.running) {
-    this.setState({timer:this.state.timer2});
+  if(this.state.isRunning) {
+    this.setState({timer:this.state.totalElapsedTime});
     clearInterval(this.interval);
-
-
-    this.setState({running: false,flag:true});
+    this.setState({isRunning: false});
     return;
+  }
+  this.setState({startTime: new Date()}); // Starting time
+  this.interval = setInterval(() => {
+    this.setState({
+    timeElapsed: new Date() - this.state.startTime,
+    totalElapsedTime: this.state.timer + this.state.timeElapsed,
+    isRunning: true });
+  },80);
 }
 
-  this.setState({startTime: new Date(),
-    totalElapsedTime:this.state.totalElapsedTime + this.state.timeElapsed});
-  this.setState({flag: true});
-  this.interval = setInterval(() => {
-          this.setState({
-          timeElapsed: new Date() - this.state.startTime,
-          timer2: this.state.timer + this.state.timeElapsed,
-          running: true });
-     },80);
-  }
-
-  appsTitle(){
-    return(
-      <View style={{borderBottomWidth: 0.5,paddingTop: 10,paddingBottom: 0,backgroundColor: '#F9F9F9'}}>
+appsTitle(){
+  return(
+    <View style={{borderBottomWidth: 0.5,paddingTop: 10,paddingBottom: 0,backgroundColor: '#F9F9F9'}}>
          <Text style={{alignSelf: 'center',fontWeight: '600',fontSize: 25,}}>Stopwatch</Text>
-      </View>
+    </View>
     );
-  }
+}
 
-  render(){
-    return (
-      <View style={{flex:1,justifyContent:'space-between'}}>
-          <View style={{flex:1}}>
-              {this.appsTitle()}
-          </View>
-          <View style={{flex: 1,backgroundColor: '#FFFFFF',justifyContent: 'center',alignItems:'flex-end'}}>
-             <Text style={{fontSize: 70,alignSelf:'center'}}>
-                {FormatTime(this.state.timer2)}{'\n'}
-             </Text>
-          </View>
-          <View style={{flex:1}} >
-              {this.startStopButton()}
-          </View>
+render(){
+  return (
+    <View style={{flex:1,justifyContent:'space-between'}}>
+      <View style={{flex:1}}>
+        {this.appsTitle()}
       </View>
+      <View style={{flex: 1,backgroundColor: '#FFFFFF',justifyContent: 'center',alignItems:'flex-end'}}>
+        <Text style={{fontSize: 70,alignSelf:'center'}}>
+          {FormatTime(this.state.totalElapsedTime)}{'\n'}
+        </Text>
+      </View>
+      <View style={{flex:1,paddingTop:100}} >
+        {this.startStopButton()}
+      </View>
+    </View>
     );
   }
 }
@@ -120,4 +112,5 @@ export const styles = StyleSheet.create({
     alignItems: 'center'
   }
 });
+
 AppRegistry.registerComponent('stopwatch', () => stopwatch);
